b2fc372c66841955a7662ad7c2a896edae850975
Mukhammad-AbuHafsa <muhammad14012007@gmail.com>
2026-07-09 01:00:41 +0300

    Add JavaScript functions

---
+++ main.js
@@ -0,0 +1,51 @@
+// Покраска первой карточки
+
+const productCard = document.querySelector('.card');
+const btnColor = document.querySelector('[data-action="color"]');
+
+btnColor.addEventListener('click', () => {
+    console.log('Кнопка была нажата');
+    productCard.style.backgroundColor = '#d59fca'
+    });
+
+
+// Покраска всех карточек
+
+const productCards = document.querySelectorAll('.card');
+const btnColorAll = document.querySelector('[data-action="color-all"]');
+
+btnColorAll.addEventListener('click', () => {
+    productCards.forEach((item) => item.style.backgroundColor = '#8cd7df')
+});
+
+
+// Открыть Google
+
+const btnOpenGoogle = document.querySelector('[data-action="google"]');
+btnOpenGoogle.addEventListener('click', openGoogle);
+
+function openGoogle() {
+    const answer = confirm('Вы действительно хотите открыть Google?');
+
+    if (answer) {
+        window.open('https://google.com')
+    } else {
+        return
+    }
+}
+
+
+// Вывод контента заголовка в консоль при наведении
+
+const getTitle = document.querySelector('.products__title');
+
+getTitle.addEventListener('mouseenter', (event) => {
+    console.log(event.target.textContent);
+});
+
+const buttonCustomizable = document.querySelector('[data-action="customizable"]');
+
+buttonCustomizable.addEventListener('click', () => {
+    console.log('Поменялся цвет')
+    buttonCustomizable.classList.toggle('btn--purple');
+});
